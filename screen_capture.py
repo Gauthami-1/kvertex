8c93f53674fd7b10d2609b032d117cd6708046f7
Gauthami-1 <93447278+Gauthami-1@users.noreply.github.com>
2025-02-15 13:08:44 -0600

    edited to generalize code

--- screen_capture.py
+++ screen_capture.py
@@ -3,28 +3,30 @@ import numpy as np
 from time import time
 from mss import mss
 
-#stores current time to calculate FPS in loop
 loop_time = time()
 
 with mss() as sct:
-    monitor = {"top": (3024//2)-50, "left": (1964//2)-50, "width": 100, "height": 100}   # middle 100 pixels
+    screen_width = sct.monitors[1]["width"]
+    screen_height = sct.monitors[1]["height"]
 
-    while(True):
+    monitor = {
+        "top": screen_height // 2 - 50,
+        "left": screen_width // 2 - 50,
+        "width": 100,
+        "height": 100
+    }
 
-        screenshot = np.array(sct.grab(monitor))  #captures screenshot
-        screenshot = cv.cvtColor(screenshot, cv.COLOR_RGB2BGR)  #convertes image from RGB to BGR (needed for OpenC
+    while True:
+        screenshot = np.array(sct.grab(monitor))
+        screenshot = cv.cvtColor(screenshot, cv.COLOR_RGB2BGR)
 
-        #new window shows live feed
         cv.imshow('Computer Vision', screenshot)
 
-        #infinite loop
-        print('FPS {}'.format(1 / (time() - loop_time)))
+        print('FPS {:.2f}'.format(1 / (time() - loop_time)))
         loop_time = time()
 
-        #stop when q is pressed 
         if cv.waitKey(1) == ord('q'):
             cv.destroyAllWindows()
             break
-
 
 print('Done.')
